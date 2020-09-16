1c6721762eb32f2ebe3d2d3b33134d29965b8dbc
Roberto Ciatti <gekorob@users.noreply.github.com>
2020-09-16 09:48:33 +0200

    Modified to adapt to USB-C Dock Gen-2

--- bin/i3cmds/displays_for_home_dock.sh
+++ bin/i3cmds/displays_for_home_dock.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-docking_uuid='0048e9b8-c99c-0801-ffff-ffffffffffff'
-device_list_filtered=$(boltctl list | grep $docking_uuid)
+product="ThinkPad USB-C Dock Gen2"
+usb-devices | grep "$product"
 
-if [[ $device_list_filtered == *"$docking_uuid"* ]]; then 
+if [[ $? -eq 0 ]]; then 
   source $HOME/.screenlayout/home_docking.sh
 fi
